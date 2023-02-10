68bf5cfd3956617baf2c3c50a21a64e3a7f52724
Tahir iqbal <shahtahir786@gmail.com>
2023-02-10 23:05:16 +0530

    Initial files of my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+
+    container: {
+      center: "true",
+    },
+    extend: {
+
+      fontFamily: {
+        FontBody: "Prompt",
+        FontLogo: "Arizonia",
+      },
+
+      animation: {
+        blob: "blob 7s infinite",
+      },
+
+      keyframes: {
+        blob: {
+          "0": {transform: "translate(0px, 0px) scale(1)"},
+          "33%": {transform: "translate(30px, -50px) scale(1.1)"},
+          "66%": {transform: "translate(-20px, 20px) scale(0.9)"},
+          "100%": {transform: "translate(0px, 0px) scale(1)"},
+        },
+      },
+    },
+  },
+  plugins: [],
+}
