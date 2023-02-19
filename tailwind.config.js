18672beb391f0c39baef624e708340430204cef8
Tahir iqbal <shahtahir786@gmail.com>
2023-02-19 21:08:00 +0530

    added footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     screens: {
-      sm: "480px",
+      sm: {"max":"480px"},
       md: "768px",
       lg: "976px",
       xl: "1440px"
@@ -12,7 +13,36 @@ module.exports = {
     container: {
       center: "true",
     },
+
     extend: {
+
+      fontFamily:{
+        body: "Poppins",
+        header: "Prompt",
+      },
+
+      textColor: {
+        skin: {
+          base: "var(--color-text-base)",
+          muted: "var(--color-text-muted)",
+          inverted: "var(--color-text-inverted)",
+        }
+      },
+
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-fill)",
+          "button-accent": "var(--color-button-accent)",
+          "button-accent-hover": "var(--color-button-accent-hover)",
+          "button-muted": "var(--color-button-muted)",
+        }
+      },
+
+      gradientColorStops: {
+        skin: {
+          hue: "var(--color-fill)",
+        }
+      },
 
       animation: {
         blob: "blob 7s infinite",
